d6aa999d1a40681ec413c8273ca1a9441e44a0e2
EC2 Default User <ec2-user@ip-172-31-21-103.us-east-2.compute.internal>
2020-05-19 16:06:07 +0000

    add my second py files

---
+++ lec2.py
@@ -0,0 +1,34 @@
+"""
+Your module description
+"""
+"""
+this is my second py code
+
+for my second lecture
+"""
+
+#print ('hello world') # this is a single line commment 
+
+# this is my second line comment 
+
+#print(type("123."))
+
+#print ("Hello World".upper())
+#print("Hello World".lower())
+
+#print("hello" + "world" + ".")
+
+#print(2**3)
+
+#my_str = "hello world"
+#print(my_str)
+
+#my_str = "Tom"
+#print(my_str)
+
+
+my_int = 2
+
+my_float = 3.0 
+
+print(my_int + my_float)
