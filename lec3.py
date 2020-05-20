d940bf0bde6790fa375605261d78f9ab37f60d0b
EC2 Default User <ec2-user@ip-172-31-21-103.us-east-2.compute.internal>
2020-05-20 16:04:18 +0000

    day 3 py files

---
+++ lec3.py
@@ -0,0 +1,42 @@
+"""
+lecture notes for day3 week1
+"""
+my_list = [1,2,3,4,5]
+my_letters = ['a','b','c']
+my_list[0]=6 #replaced the first integer with the number 6 (0=6)
+#my_list.append(7) #added number 7 to my list
+#my_list.remove(7) #removed 7 from my list 
+print(my_list)
+
+#print(len(my_list))
+
+#print(6 in my_list)
+#print('6' in my_list)
+
+#my_list.extend(['a','b'])
+
+#print(my_list+ [8,9])
+
+#my_list.sort()
+#print(my_list)
+
+#my_list.reverse()
+#print(my_list)
+
+#print(my_list[:])
+
+#print(my_nested_list)
+
+#print('helloworld')
+#print('helloworld'[9])
+#print('hello\nworld')
+
+my_letters = ['a','a','b','b','c']
+print(my_letters)
+# print(set(my_letters)) 
+
+my_unique_letters = set(my_letters)
+print(list(my_unique_letters))
+#print('d' in my_unique_letters) 
+
+#print(len('hello world'))
