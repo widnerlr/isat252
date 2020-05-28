43d08229559e86a4833786f732889bae8d30b12b
EC2 Default User <ec2-user@ip-172-31-21-103.us-east-2.compute.internal>
2020-05-28 15:28:05 +0000

    5/28 files

---
+++ lab7.py
@@ -0,0 +1,72 @@
+"""
+lab 7
+"""
+
+#3.1
+
+#i=7
+
+#while i >=1:
+#    i = i-1 
+#    
+#    if i == 3:
+#        continue
+#    if i == 6:
+#      continue
+#    print(i)
+    
+#3.2
+
+#i = 1
+#result=1    
+#while i <=5: 
+#    result=result*i 
+#    i=i+1
+#print(result)
+
+#3.3
+
+#i=1
+#result=0
+#while i<=5:
+#    result=result+i
+#    i=i+1
+#print(result)
+
+#3.4 
+
+#i=3
+#result=3
+#while i<=8:
+#    #print(i)
+ #   result=result*i
+ #   i=i+1
+#print(result)
+
+#3.5
+
+#i=4
+#result=1
+#while i<=8:
+    #print(i)
+  #  result=result*i
+  #  i=i+1
+#print(result)
+
+#3.6
+
+num_list = [ 12, 32, 43, 35 ]
+
+while num_list:
+    num_list.remove(num_list[0])
+
+print(num_list)
+
+
+
+    
+
+
+    
+
+    
---
+++ lec7.py
@@ -0,0 +1,37 @@
+"""
+Your module description
+"""
+
+i = 5 
+
+#while i >= 0 :
+#
+ ##  if i == 3: 
+   #     pass 
+    #print(i) 
+    
+    
+#try:
+ #   print(1/0)
+#except(ZeroDivisionError):
+ #   print('division by zero')
+
+    
+#for word in "hello world".split():
+ #   print(word)
+    
+  #  for str_item in word:
+        
+   #     if str_item == "l":
+    #        break
+     #   print(str_item)
+        
+i = 5 
+while i >=0:
+    
+    try:
+        print(1/(i-3))
+    except:
+        pass
+    
+    i=i-1
